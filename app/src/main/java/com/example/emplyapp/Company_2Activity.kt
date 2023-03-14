c25d6d0782ad532524b0b1761465e269c3e7061a
babymwc <warinchaya.c@kkumail.com>
2023-03-14 20:48:57 +0700

    mwc.model kaaaaaaa

---
+++ app/src/main/java/com/example/emplyapp/Company_2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.emplyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Company_2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_company2)
+    }
+}
